e29a4bb29923eb32bc523b77a84908558a513334
BBeumseok <bbseok97@naver.com>
2024-08-01 08:11:54 +0000

    frontend deployment

--- .platform/hooks/prebuild/01_configure_swap_space.sh
+++ .platform/hooks/prebuild/01_configure_swap_space.sh
@@ -5,3 +5,8 @@ if [ -f $SWAPFILE ]; then
   echo "$SWAPFILE found, skip"
   exit;
 fi
+
+/bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=2048
+/bin/chmod 600 $SWAPFILE
+/sbin/mkswap $SWAPFILE
+/sbin/swapon $SWAPFILE
