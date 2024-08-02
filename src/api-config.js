acc81028ba1f65b63780ae6a83b41b908f658f81
BBeumseok <bbseok97@naver.com>
2024-08-02 03:57:43 +0000

    frontend deployment

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 }else {
-  backendHost = "dev.bumseok.r-e.kr"
+  backendHost = "http://dev.bumseok.r-e.kr/"
 }
 
 export const API_BASE_URL = `${backendHost}`;
