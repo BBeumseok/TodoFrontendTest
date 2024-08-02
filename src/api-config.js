4deb09c13c53500fcd686b1cd687d23a058dcf76
BBeumseok <bbseok97@naver.com>
2024-08-02 03:52:45 +0000

    frontend deployment

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 }else {
-  backendHost = "http://todoapplication128-backend-dev.ap-northeast-2.elasticbeanstalk.com"
+  backendHost = "dev.bumseok.r-e.kr"
 }
 
 export const API_BASE_URL = `${backendHost}`;
